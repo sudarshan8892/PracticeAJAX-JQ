a2a3c224f63e535139749a665555a857c05003e6
mforazure9@gmail.com <mforazure9@gmail.com>
2024-03-17 19:24:35 +0530

    Add project files.

---
+++ login-usingAjaxJQ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace login_usingAjaxJQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
